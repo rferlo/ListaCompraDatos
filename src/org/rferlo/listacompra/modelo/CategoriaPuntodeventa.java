c70db4288c4ca17cb8c894ea5e86db756596b456
Rafael Fernández <rferlo@gmail.com>
2013-05-13 22:49:55 +0200

    00

---
+++ src/org/rferlo/listacompra/modelo/CategoriaPuntodeventa.java
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+package org.rferlo.listacompra.modelo;
+
+/**
+ * @author Rafa
+ *
+ */
+public class CategoriaPuntodeventa implements Categoria {
+
+	private String nombre;
+	private String abreviatura;
+	
+	/* (non-Javadoc)
+	 * @see org.rferlo.listacompra.modelo.Categoria#getNombre()
+	 */
+	@Override
+	public String getNombre() {
+		return nombre;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.rferlo.listacompra.modelo.Categoria#getAbreviatura()
+	 */
+	@Override
+	public String getAbreviatura() {
+		return abreviatura;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.rferlo.listacompra.modelo.Categoria#setNombre(java.lang.String)
+	 */
+	@Override
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.rferlo.listacompra.modelo.Categoria#setAbreviatura(java.lang.String)
+	 */
+	@Override
+	public void setAbreviatura(String abreviatura) {
+		this.abreviatura = abreviatura;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.rferlo.listacompra.modelo.Categoria#empiezapor(java.lang.String)
+	 */
+	@Override
+	public boolean empiezapor(String cadena) {
+		if(null != nombre) {
+			if(nombre.startsWith(cadena)) {
+				return true;
+			} else {
+				if(null != abreviatura) {
+					return abreviatura.startsWith(cadena);
+				}
+			}
+		}
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.rferlo.listacompra.modelo.Categoria#contiene(java.lang.String)
+	 */
+	@Override
+	public boolean contiene(String cadena) {
+		if(null != nombre) {
+			if(nombre.contains(cadena)) {
+				return true;
+			} else {
+				if(null != abreviatura) {
+					return abreviatura.contains(cadena);
+				}
+			}
+		}
+		return false;
+	}
+
+}
